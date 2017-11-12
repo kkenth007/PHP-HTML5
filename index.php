c3fa9b9a927db2c0d2b4f8edd6d8ace768de4506
Kanit Wijitpanya <32562129+kkenth007@users.noreply.github.com>
2017-11-12 21:24:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sed</title>
+  </head>
+  <body>
+
+    <form action="kenichi.php" method="post">
+    <table border="5">
+      <tr>
+      <td>Column:</td>
+      <td><input type="text" name="col"></td>
+      </tr>
+
+      <tr>
+      <td>Row:</td>
+      <td><input type="text" name="row"></td>
+      </tr>
+
+      <tr>
+      <td>Color:</td>
+      <td><input type="color" name="fav"></td>
+      </tr>
+
+      <tr>
+      <td align ="left"colspan="2"> <button type="submit" value="Submit">Submit</button>
+      <button type="reset" value="Reset">Reset</button></td>
+      </tr>
+      </table>
+      </form>
+
+  </body>
+</html>
